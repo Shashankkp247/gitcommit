7028485ff7c5e685e20d5a4a784dac6b59c8fe7c
Shashankkp247 <90641020+Shashankkp247@users.noreply.github.com>
2022-01-14 13:04:32 +0530

    so

--- hello.py
+++ hello.py
@@ -1,2 +1,2 @@
 print("Hello this is my repo for testing git skills on Terminal.\nSo I am writing this on 12:31 on 14-01-2022 So if u see this in my repo then I have already commited and pushed my file using : git commit \nand if no merge conflicts or any other erorrs then after I can execute : \ngit push")
-hi = "IDK"
+hi = "IDKso"
